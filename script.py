e8652e829477b05f14bb4c34946afc947d2e3c50
codecademy <ccuser@codecademy.com>
2019-10-22 19:29:01 +0000

    Added logic to find travler destinations and convert to internal data

--- script.py
+++ script.py
@@ -2,3 +2,21 @@
 destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'Sao Paulo, Brazil', 'Cairo, Egypt']
 #Define a traveler
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+
+# Testing the function
+#print(get_destination_index('Los Angeles, USA'))
+#print(get_destination_index('Paris, France'))
+#print(get_destination_index('Hyderabad, India')) # Value Error is returned as expected TODO: Add an error handler
+
+def get_traveler_location(traveler):
+  traveler_destination = traveler[1]
+  traveler_destination_index = get_destination_index(traveler_destination)
+  return traveler_destination_index
+#Testing get_traveler_location function
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
